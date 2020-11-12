ae21a4f5e9b03b39767c7c0bfe0c2412235ddd04
Jiahong Long <jiahong888long@gmail.com>
2020-11-12 21:06:05 +0000

    Sync LeetCode submission - Permutations II (python)

---
+++ problems/permutations_ii/solution.py
@@ -0,0 +1,10 @@
+import itertools as it
+
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        return list(set(it.permutations(nums)))
